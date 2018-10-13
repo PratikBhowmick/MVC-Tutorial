214abb3509d2abbf66e3f784266b92a101024e8a
Pratik <pratik.bhowmick@yahoo.co.in>
2018-10-14 04:01:45 +0530

    Making the datetime field look good in dd/mm/yyyy format

--- Repo/DatabaseFirstApproach/OrganizationDetailscp/OrganizationDetails/Models/Employee.cs
+++ Repo/DatabaseFirstApproach/OrganizationDetailscp/OrganizationDetails/Models/Employee.cs
@@ -11,7 +11,8 @@ namespace OrganizationDetails.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Employee
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,6 +24,7 @@ namespace OrganizationDetails.Models
         public int EmployeeID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy}",ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> JoiningDate { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
